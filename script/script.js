89765658c1d690f11fe4b402c28e919d1c06fe12
Krit2124 <kritserg2124@gmail.com>
2023-04-25 12:55:03 +0300

    adaptive 600px width

--- script/script.js
+++ script/script.js
@@ -1,3 +1,11 @@
 $(function(){
     $("#input-phone").mask("8 (999) 999-99-99")
 })
+
+const burger = document.querySelector('.burger')
+const links = document.querySelector('.header-links');
+
+function toggleMenu() {
+    burger.classList.toggle('open')
+    links.classList.toggle('open')
+}
